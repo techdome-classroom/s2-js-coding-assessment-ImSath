2f977b54cac1be620c53ea0829014579a87b2c54
sathwikha yadav <kssathwikha1259@gmail.com>
2024-11-11 15:52:59 +0530

    solution commited

--- program1.js
+++ program1.js
@@ -3,7 +3,21 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    const bracketMap = { ')': '(', '}': '{', ']': '[' };
+    const stack = [];
     
+    for (let char of s) {
+        if (bracketMap[char]) {
+            const topElement = stack.length > 0 ? stack.pop() : '#';
+            if (topElement !== bracketMap[char]) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+    
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
